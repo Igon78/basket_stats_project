e5957c594030607599b921d794d7fb36ebd857d6
Igon78 <xpiotek2000x@gmail.com>
2023-01-16 20:36:08 +0100

    Standings

--- NBA.py
+++ NBA.py
@@ -1,19 +1,22 @@
 import io
+from urllib.request import urlopen
+
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.endpoints import shotchartdetail
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import leaguestandings
+
 import tkinter as tk
 from tkinter import ttk, BOTH
 from tkinter import Frame, Label, Button
-from urllib.request import urlopen
-
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from nba_api.stats.endpoints import playercareerstats
-from nba_api.stats.endpoints import shotchartdetail
 import tkinter.messagebox
 from PIL import Image, ImageTk
 from ttkwidgets.autocomplete import AutocompleteCombobox
-from nba_api.stats.static import players
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle, Rectangle, Arc
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 pd.set_option('display.max_columns', None)
 
 
@@ -23,18 +26,16 @@ def createPlayers():
     imageLabel = Label(imageFrame)
     imageLabel.pack()
 
-    searchFrame = Frame(playersFrame, bg='#ffffff')  # frame dotyczący wyszukiwania zawodnika
-    searchFrame.place(relx=0.5, rely=0.5, relheight=0.1, relwidth=0.3, anchor='n')
-    playersArray = pd.DataFrame(
-        players.get_active_players()).full_name.values.tolist()  # pobranie listy nazwisk graczy do wyszukiwania
-    searchEntry = AutocompleteCombobox(searchFrame, width=30,
-                                       completevalues=playersArray)  # widget do wyboru zawodnika z autokompletacją wyszukiwania
-    searchEntry.pack()
-
+    tree = ttk.Treeview(playersFrame, show='headings')# stworzenie tabeli
+    tree.place(relx=0, rely=0.7, relheight=0.2, relwidth=1)
+    columns = ['SEASON_ID', 'GP', 'MIN', 'PTS', 'PF','TOV', 'BLK', 'STL', 'AST', 'OREB', 'DREB']
+    tree.configure(columns=columns)
+    for column in columns:
+        tree.column(column, minwidth=0, width=55)
+        tree.heading(column, text=column)
     def showPlayerStats():  # funkcja do wyświetlenia informacji o graczu
         # pobranie id gracza potrzebnego do pobrania statystyk oraz zdjęcia gracza
         player_id = pd.DataFrame(players.find_players_by_full_name(searchEntry.get())).id[0]
-
         photo_url = f"https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/{player_id}.png"
         url = urlopen(photo_url)
         image_raw = io.BytesIO(url.read())  # pobranie zdjęcia z linku
@@ -43,42 +44,52 @@ def createPlayers():
         url.close()
         imageLabel.configure(image=image_tk)  # ustawienie zdjęcia w widgetcie label
         imageLabel.image = image_tk  # bez tej operacji garbage collector usuwa zdjęcie
+        #pobranie danych o statystykach gracza
         season_stats = playercareerstats.PlayerCareerStats(player_id=player_id).get_data_frames()[0]
         season_stats = season_stats[['SEASON_ID', 'GP', 'MIN', 'PTS', 'PF',
                                      'TOV', 'BLK', 'STL', 'AST', 'OREB', 'DREB']]
         season_stats = season_stats[season_stats.SEASON_ID == '2022-23']
-        columns = season_stats.columns.tolist()
+
         try:
-            tree.delete('id')
+            tree.delete('row') # usunięcie danych z tabeli dotyczących wcześniej wyszukanego zawodnika
         except Exception:
             pass
-        tree.configure(columns=columns)
-        for column in columns:
-            tree.column(column, minwidth=0, width=55)
-            tree.heading(column, text=column)
         for index, row in season_stats.iterrows():
-            id = tree.insert("", 'end', 'id', values=list(row))
+            id = tree.insert("", 'end', 'row', values=list(row)) #dodanie danych do tabeli
 
+    searchFrame = Frame(playersFrame, bg='#ffffff')  # frame dotyczący wyszukiwania zawodnika
+    searchFrame.place(relx=0.5, rely=0.5, relheight=0.1, relwidth=0.3, anchor='n')
+    playersArray = pd.DataFrame(players.get_active_players()).full_name.values.tolist()  # pobranie listy nazwisk graczy do wyszukiwania
+    searchEntry = AutocompleteCombobox(searchFrame, width=30,
+                                       completevalues=playersArray)  # widget do wyboru zawodnika z autokompletacją wyszukiwania
+    searchEntry.pack()
     searchButton = Button(searchFrame, text="Get player data", command=showPlayerStats)
     searchButton.place(relx=0, rely=0.5, relheight=0.5, relwidth=1)
-
-    tree = ttk.Treeview(playersFrame, show='headings')
-    tree.place(relx=0, rely=0.7, relheight=0.2, relwidth=1)
 
     playersFrame.pack_forget()
 
 def createHeatMap():
-    searchFrame = Frame(heatmapFrame, bg='#ffffff')  # frame dotyczący wyszukiwania zawodnika
+    searchFrame = Frame(heatmapFrame, bg='')  # frame dotyczący wyszukiwania zawodnika
     searchFrame.place(relx=0.2, rely=0.1, relheight=0.1, relwidth=0.3, anchor='n')
     playersArray = pd.DataFrame(
         players.get_active_players()).full_name.values.tolist()  # pobranie listy nazwisk graczy do wyszukiwania
     searchEntry = AutocompleteCombobox(searchFrame, width=30,
                                        completevalues=playersArray)  # widget do wyboru zawodnika z autokompletacją wyszukiwania
     searchEntry.pack()
-    plotFrame = Label(heatmapFrame, bg='blue')
-    plotFrame.place(relx=0.65, rely=0.1, relheight=0.6, relwidth=0.55, anchor='n')
-    def show_shot_graph(): #funkcja zapożyczona ze strony https://notebook.community/bradleyfay/py-Goldsberry/docs/Visualizing%20NBA%20Shots%20with%20py-Goldsberry
-        def draw_court(ax=None, color='black', lw=2, outer_lines=False):
+
+    plotLabel = Label(heatmapFrame, bg='#ffffff')
+    plotLabel.place(relx=0.65, rely=0.1, relheight=0.6, relwidth=0.55, anchor='n')
+    def show_shot_graph():
+
+        # pobranie i przygotowanie danych o rzutach zawodnika z wybranego sezonu
+        player_id = pd.DataFrame(players.find_players_by_full_name(searchEntry.get())).id[0]
+        shoot_stats = shotchartdetail.ShotChartDetail(player_id=player_id,season_nullable="2021-22",team_id=0,context_measure_simple='FGA',
+                    season_type_all_star = ['Regular Season', 'Playoffs']).get_data_frames()[0]
+        shoot_stats=shoot_stats[['EVENT_TYPE','LOC_X','LOC_Y']]
+        shoot_stats['EVENT_TYPE']= shoot_stats['EVENT_TYPE'].apply(lambda x:0 if x=='Missed Shot' else 1)
+
+        #stworzenie wykresu
+        def draw_court(ax=None, color='black', lw=2, outer_lines=False): #funkcja zapożyczona ze strony https://notebook.community/bradleyfay/py-Goldsberry/docs/Visualizing%20NBA%20Shots%20with%20py-Goldsberry
             # If an axes object isn't provided to plot onto, just get current one
             if ax is None:
                 ax = plt.gca()
@@ -145,48 +156,83 @@ def createHeatMap():
                 ax.add_patch(element)
 
             return ax
-        player_id = pd.DataFrame(players.find_players_by_full_name(searchEntry.get())).id[0]
-        shoot_stats = shotchartdetail.ShotChartDetail(player_id=player_id,team_id=0,context_measure_simple='FGA',
-                    season_type_all_star = ['Regular Season', 'Playoffs']).get_data_frames()[0]
-        shoot_stats=shoot_stats[['EVENT_TYPE','LOC_X','LOC_Y']]
-        shoot_stats['EVENT_TYPE']= shoot_stats['EVENT_TYPE'].apply(lambda x:0 if x=='Missed Shot' else 1)
         fig = plt.figure(figsize=(12,11))
-        plt.scatter(shoot_stats.LOC_X,shoot_stats.LOC_Y,c=shoot_stats.EVENT_TYPE,cmap='coolwarm_r')
-        draw_court(outer_lines=False,color="black")
-        plt.xlim(300,-300)
+        plt.hexbin(x=shoot_stats.LOC_X, y=shoot_stats.LOC_Y, C=shoot_stats.EVENT_TYPE, gridsize=40,  cmap='Blues')
+        draw_court(outer_lines=False,color="black") #narysowanie boiska
+        plt.xlim(-300,300)
+        plt.ylim(422.5, -47.5) #limity wielkości wykresu pozwalające zachować odpowiednie proporcje boiska
         cur_axes = plt.gca()
         cur_axes.axes.get_xaxis().set_visible(False)
-        cur_axes.axes.get_yaxis().set_visible(False)
-        plotCanvas = FigureCanvasTkAgg(fig,plotFrame)
+        cur_axes.axes.get_yaxis().set_visible(False) #usunięcie niepotrzebnych osi
+
+        for widget in plotLabel.winfo_children(): #usunięcie poprzednio wygenerowanego wykresu jeśli taki powstał
+            widget.destroy()
+        plotCanvas = FigureCanvasTkAgg(fig,plotLabel) #umieszczenie wykresu w plotLabel
         plotCanvas.get_tk_widget().pack(side = tk.TOP,fill=tk.BOTH,expand=True)
+
     searchButton = Button(searchFrame, text="Get player data",command=show_shot_graph)
     searchButton.place(relx=0, rely=0.5, relheight=0.5, relwidth=1)
+
     heatmapFrame.pack_forget()
 
+def createStandings():
+    # stworzenie tabel
+    tree_west = ttk.Treeview(standingsFrame, show='headings')
+    tree_west.place(relx=0, rely=0.15, relheight=0.85, relwidth=0.5)
+    tree_east = ttk.Treeview(standingsFrame, show='headings')
+    tree_east.place(relx=0.5, rely=0.15, relheight=0.85, relwidth=0.5)
+    columns = ['Rank','Team','Record','WinPCT','L10']
+    tree_west.configure(columns=columns)
+    tree_east.configure(columns=columns)
+    for column in columns:
+        tree_west.column(column, minwidth=0, width=20)
+        tree_west.heading(column, text=column)
+        tree_east.column(column, minwidth=0, width=20)
+        tree_east.heading(column, text=column)
+    #pobranie danych
+    stands = leaguestandings.LeagueStandings(season="2022-23").get_data_frames()[0][['PlayoffRank',
+                'TeamName','Conference','Division','Record','WinPCT','L10']]
+    stands_west = stands[stands['Conference'] == 'West']
+    stands_west.drop(columns=['Conference','Division'], inplace=True)
+    stands_east = stands[stands['Conference'] == 'East']
+    stands_east.drop(columns=['Conference','Division'], inplace=True)
+    #dodanie danych do tabeli
+    for index, row in stands_west.iterrows():
+        tree_west.insert("", 'end', values=list(row))
+    for index, row in stands_east.iterrows():
+        tree_east.insert("", 'end', values=list(row))
+
+def createTeams():
+    pass
 root = tkinter.Tk() #stworzenie okienka
 root.title('NBA stats')
 root.geometry('600x400')
 root.grid_rowconfigure(0,weight=1)
 root.grid_columnconfigure(0,weight=1)
 
-
 playersFrame = Frame(root, bg='#ffffff')
 playersFrame.grid(row=0,column=0,sticky="nsew")
 
-teamsFrame  = Frame(root, bg='#ffffff')
+teamsFrame = Frame(root, bg='#ffffff')
 teamsFrame.grid(row=0,column=0,sticky="nsew")
 
 heatmapFrame = Frame(root, bg='#ffffff')
 heatmapFrame.grid(row=0,column=0,sticky="nsew")
+
+standingsFrame = Frame(root,bg='#ffffff')
+standingsFrame.grid(row=0,column=0,sticky="nsew")
 
 def create_menu():
 
     menubar = tk.Menu(root)
     createPlayers()
     createHeatMap()
+    createTeams()
+    createStandings()
     menubar.add_command(label='Players',command=playersFrame.tkraise)
     menubar.add_command(label='Teams',command=teamsFrame.tkraise)
     menubar.add_command(label='Shot heatmap',command=heatmapFrame.tkraise)
+    menubar.add_command(label='Standings',command=standingsFrame.tkraise)
     root.config(menu=menubar)
 
 create_menu()
